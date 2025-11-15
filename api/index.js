c9d238924cd67d17f33cc93a07257671eaa231fb
virajrajguru67-png <virajrajguru67@gmail.com>
2025-11-15 18:34:11 +0530

    Remove compiled API files and update gitignore to prevent conflicts

--- api/index.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from '../src/app.js';
-// Create Express app instance
-const app = createApp();
-// Export as Vercel serverless function
-export default function handler(req, res) {
-    return app(req, res);
-}
-//# sourceMappingURL=index.js.map
